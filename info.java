9cbe5b05e210c5a41114f190c7f45bc04d2e307b
martuzaiu <martuza.cse@gmail.com>
2014-10-19 21:30:25 +0600

    java file added

---
+++ info.java
@@ -0,0 +1,28 @@
+Member 1:
+Name: Md. Martuza Ahamad
+Email: martuza.cse@gmail.com
+Phone: 01723847749
+Speciality: Coding
+Year of Study: 4th
+University: Islamic University
+Member 2.
+Name: Sakib Hasan
+Email: sakibsumon@gmail.com
+Phone: 01731112015
+Speciality: Coding
+Year of Study: 4th
+University: Islamic University
+Member 3.
+Name: Jobayer Alam
+Email: jobayercseiu91@gmail.com
+Phone: 01737916791
+Speciality: Idea
+Year of Study: 4th
+University: Islamic University
+Member 4.
+Name: MD Shahabub Alam
+Email: tutulcsema@gmail.com
+Phone: 01738622490
+Speciality: Designing
+Year of Study: 4th
+University: Islamic University
